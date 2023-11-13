8325347eb4178d0e278bbb49f3505c10c10444be
Ackey-code <1986ackey@gmail.com>
2023-11-14 02:54:59 +0900

    +7 bottom, +start_height, H=5 S=2

--- src/run-model-api.py
+++ src/run-model-api.py
@@ -29,6 +29,7 @@ def adjust_list(lst):
             adjusted_list.append(s + '-'*(100 - len(s)))
         else:
             adjusted_list.append(s[:100])
+    adjusted_list.extend(['-' * 100 for _ in range(7)])
     return adjusted_list
 
 def check_columns(lst):
@@ -62,7 +63,6 @@ def process_neighbour(i, j, grid, object_cells):
     process_neighbour(i, j+1, grid, object_cells)
     process_neighbour(i, j-1, grid, object_cells)
 
-
 def create_json_file(env_list):
     grid = [list(row) for row in env_list]
     objects_dict = dict()
@@ -71,6 +71,13 @@ def create_json_file(env_list):
     grid_width = len(grid[0])
     grid_height = len(grid)
 
+    # Initialize start_height with the minimum value
+    start_height = 1  # Start from the bottom row (1)
+    for i in range(grid_height):
+        for j in range(min(5, grid_width)):  # Check only the first 5 columns
+            if grid[i][j] == 'H' or grid[i][j] == 'S':
+                start_height = max(start_height, grid_height - i)
+
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 'H' or grid[i][j] == 'S':
@@ -78,8 +85,8 @@ def create_json_file(env_list):
 
                 process_neighbour(i, j, grid, new_object_indices)
 
-                new_object_types = [1 if grid[(len(grid)-1-idx//grid_width)][idx%grid_width] == 'H' else 2 for idx in new_object_indices]
-
+                # Modified: 'H' is represented as 5 and 'S' as 2
+                new_object_types = [5 if grid[(len(grid)-1-idx//grid_width)][idx%grid_width] == 'H' else 2 for idx in new_object_indices]
                 new_object = {}
                 new_object['indices'] = new_object_indices
                 new_object['types'] = new_object_types
@@ -94,6 +101,7 @@ def create_json_file(env_list):
     return_dict = {}
     return_dict['grid_width'] = grid_width
     return_dict['grid_height'] = grid_height
+    return_dict['start_height'] = start_height
     return_dict['objects'] = objects_dict
 
     return return_dict
@@ -112,14 +120,51 @@ def generate_env(prompt):
 
     return json_env, fixed_list
 
+# json形式から文字列リストに逆変換するテスト用関数
+def recreate_fixed_list(json_env):
+    """
+    Recreates the fixed_list from the json_env object with inverted y-axis.
+    The json_env contains information about different objects, their indices in a grid,
+    and their types (represented as 1 for 'H' and 2 for 'S').
+    The grid is reconstructed into a list of strings, each representing a row, with an inverted y-axis.
+    """
+    grid_width = json_env['grid_width']
+    grid_height = json_env['grid_height']
+    objects = json_env['objects']
+
+    # Initialize the grid with '-' (empty space)
+    grid = [['-' for _ in range(grid_width)] for _ in range(grid_height)]
+
+    # Populate the grid with objects, inverting the y-axis
+    for obj in objects.values():
+        indices = obj['indices']
+        #obj_type = 'H' if 1 in obj['types'] else 'S'
+        # typeはここで対応させる．Hをif 5と変更
+        obj_type = 'H' if 5 in obj['types'] else 'S'
+
+        for index in indices:
+            x = index % grid_width
+            y = grid_height - 1 - (index // grid_width)  # Invert the y-axis
+            grid[y][x] = obj_type
+
+    # Convert grid rows to strings
+    fixed_list = [''.join(row) for row in grid]
+
+    return fixed_list
+
+
 def main():
-    prompt = "100*20 size Evolution Gym environment that is shaped like a mountain."
+    prompt = "100*20 size Evolution Gym environment that is difficult."
     json_env, fixed_list = generate_env(prompt)
     for s in fixed_list:
         print(s)
     print(json_env)
     with open('test.json', 'w') as f:
         json.dump(json_env, f)
+
+    re_list = recreate_fixed_list(json_env)
+    for s in re_list:
+        print(s)
 
 if __name__ == "__main__":
     main()
